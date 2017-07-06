7e8039feb0b450b30bcbec296f6eecb0217f30e9
changjiashuai <rj1031cjs@163.com>
2017-07-06 11:43:48 +0800

    add BaseFragment.

--- mvp-core/src/main/java/com/changjiashuai/mvp/core/BaseFragment.java
+++ mvp-core/src/main/java/com/changjiashuai/mvp/core/BaseFragment.java
@@ -1,30 +1,31 @@
 package com.changjiashuai.mvp.core;
 
-import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 /**
  * Email: changjiashuai@gmail.com
  *
- * Created by CJS on 2017/7/6 11:21.
+ * Created by CJS on 2017/7/6 11:36.
  */
 
+public abstract class BaseFragment<V extends BaseView, T extends BasePresenter<V>> extends Fragment implements BaseView {
 
-public abstract class BaseActivity<V extends BaseView, T extends BasePresenter<V>> extends Activity implements BaseView {
 
     protected T mPresenter;
 
     public abstract void setPresenter(T presenter);
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         mPresenter.attachView((V) this);
     }
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         super.onDestroy();
         if (mPresenter != null) {
             mPresenter.detachView();
@@ -33,11 +34,11 @@ public abstract class BaseActivity<V extends BaseView, T extends BasePresenter<V
 
     @Override
     public void showError(String error) {
-        Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), error, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void showMessage(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
     }
 }
